e4387c80351958342616ae2eeaa67b78fb93ce74
Andy Kamath <andy@andykamath.net>
2016-03-30 01:16:54 -0400

    Update scanForTrump.js

--- scanForTrump.js
+++ scanForTrump.js
@@ -3,24 +3,24 @@ a = document.getElementsByTagName('img');
 
 document.body.innerHTML = document.body.innerHTML.replace(/Great Again/gi, 'Complete Shit')
 												 .replace(/GreatAgain/gi, 'CompleteShit')
-												 .replace(/Donald Trump/gi, 'Adolf Hitler')
-												 .replace(/Donald John Trump/gi, 'Adolf Hitler')
-												 .replace(/Donald J Trump/gi, 'Adolf Hitler')
-												 .replace(/Donald J. Trump/gi, 'Adolf Hitler')
-												 .replace(/DonaldTrump/gi, 'AdolfHitler')
-												 .replace(/DonaldJTrump/gi, 'AdolfHitler')
-												 .replace(/donaldjtrump/gi, 'adolfhitler')
-												 .replace(/Trump/gi, 'Hitler')
-												 .replace(/ trump/gi, ' Hitler')
-												 .replace(/trump /gi, 'Hitler ')
-												 .replace(/trump/gi, 'hitler')
-												 .replace(/the donald/gi, 'Adolf Hitler')
-												 .replace(/the Donald/gi, 'Adolf Hitler')
-												 .replace(/The Donald/gi, 'Adolf Hitler')
-												 .replace(/the donald/gi, 'Adolf Hitler');
+												 .replace(/Donald Trump/gi, 'some dumbass')
+												 .replace(/Donald John Trump/gi, 'some dumbass')
+												 .replace(/Donald J Trump/gi, 'some dumbass')
+												 .replace(/Donald J. Trump/gi, 'some dumbass')
+												 .replace(/DonaldTrump/gi, 'somedumbass')
+												 .replace(/DonaldJTrump/gi, 'somedumbass')
+												 .replace(/donaldjtrump/gi, 'somedumbass')
+												 .replace(/Trump/gi, 'some dumbass')
+												 .replace(/ trump/gi, ' some dumbass')
+												 .replace(/trump /gi, 'some dumbass ')
+												 .replace(/trump/gi, 'some dumbass')
+												 .replace(/the donald/gi, 'some dumbass')
+												 .replace(/the Donald/gi, 'some dumbass')
+												 .replace(/The Donald/gi, 'some dumbass')
+												 .replace(/the donald/gi, 'some dumbass);
 												 
 for (var i = 0; i < l.length; i++) {
-	l[i].href.replace(/realAdolfHitler|AdolfHitler/g, 'berniesanders')
+	l[i].href.replace(/realsomedumbass|somedumbass/g, 'berniesanders')
 	
 	if (l[i].href.toLowerCase().indexOf('hitler') > -1) { 
 		l[i].href = "http://feelthebern.org";
@@ -28,7 +28,7 @@ for (var i = 0; i < l.length; i++) {
 		
 		if (images[0]) { 
 			for (var j = 0; j < images.length; j++) { 
-				images[j].src = "http://media-2.web.britannica.com/eb-media/58/129958-004-C9B8B89D.jpg";
+				images[j].src = "http://usedandthenewjimcrow.commons.gc.cuny.edu/files/2013/11/1.jpg";
 			}
 		}
 	}
@@ -36,6 +36,6 @@ for (var i = 0; i < l.length; i++) {
 
 for (var i = 0; i < a.length; i++) {
 	if (a[i].getAttribute('alt').toLowerCase().indexOf('hitler') > -1) {
-		a[i].src = "http://media-2.web.britannica.com/eb-media/58/129958-004-C9B8B89D.jpg"
+		a[i].src = "http://usedandthenewjimcrow.commons.gc.cuny.edu/files/2013/11/1.jpg"
 	}
 }
